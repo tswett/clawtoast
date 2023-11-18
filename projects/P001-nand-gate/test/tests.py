0a430a519abb24efa28251fdbf87cf7fd4ae93df
Tanner Swett <tannerswett@gmail.com>
2023-11-18 18:58:49 -0500

    Initial commit for P001 (NAND gate project)

---
+++ projects/P001-nand-gate/test/tests.py
@@ -0,0 +1,14 @@
+# Copyright 2023 Tanner Swett.
+#
+# This file is part of Clawtoast. Clawtoast is free software: you can
+# redistribute it and/or modify it under the terms of version 3 of the GNU
+# General Public License as published by the Free Software Foundation.
+#
+# Clawtoast is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+#from clawtoast import steamer
+#
+#with steamer.connect_to_nucleo() as nucleo:
+#    pass
